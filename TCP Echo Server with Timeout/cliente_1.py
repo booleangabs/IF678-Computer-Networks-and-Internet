5de12304f401ef95559e64307687a87762dca767
booleangabs <62074916+booleangabs@users.noreply.github.com>
2021-11-14 23:13:13 -0400

    TCP with timeout

---
+++ TCP Echo Server with Timeout/cliente_1.py
@@ -0,0 +1,24 @@
+import socket
+
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    client_socket.connect(("localhost", 28886))
+    while (message := input()) != "quit":
+        client_socket.sendall(message.encode())
+        try:
+            answer = client_socket.recv(1024).decode()
+            print(f"O servidor respondeu ''{answer}'")
+        except ConnectionAbortedError:
+            print("Conexão encerrada pelo servidor")
+            client_socket.close()
+            break
+    else:
+        print("Desligamento solicitado!")
+    client_socket.close()
+    
+except ConnectionRefusedError:
+    print("Não foi possível se conectar ao servidor.")
+
+    
+    
